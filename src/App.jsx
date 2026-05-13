923e322be3bfc9b3a275fcda19b98a89046c7099
Jorge <jorgealejos728@gmail.com>
2026-05-13 13:07:03 -0500

    arreglo github pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
 import { useAppContext } from './context/AppContext'
 import { CheckCircle, AlertCircle } from 'lucide-react'
 
